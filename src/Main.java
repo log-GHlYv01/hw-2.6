ffce1aa495f1d8e70bcfcfe513f3adf36a2d7ff5
LesnikovYV <Y-levl@ya.ru>
2022-10-09 15:18:51 +0500

    2.6.1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.annotation.processing.SupportedSourceVersion;
+import javax.swing.*;
 import java.util.*;
 
 
@@ -7,6 +8,8 @@ public class Main {
     public static void main(String[] ages){
         doTasc1();
         doTasc2();
+        doTasc3();
+        doTasc4();
     }
     private static void doTasc1 (){
         List<Integer> nums = genereit(20);
@@ -22,6 +25,27 @@ public class Main {
         for (Integer num : nums ) {
             if (num % 2==0){
                 System.out.println(num);
+            }
+        }
+    }
+    private static void doTasc3 (){
+        List<String> words = List.of("test", "test1","Test","test192834756","test","tes","test", "test1", "Test", "test192834756", "test", "tes", "Test", "tes");
+        Set<String> result = new HashSet<>(words);
+        System.out.println(result);
+   }
+    private static void doTasc4 () {
+        List<String> words = List.of("test", "test1", "Test", "test192834756", "test", "tes", "Test", "tes","test", "test1","Test","test192834756","test","tes","test", "test1", "Test", "test192834756", "test", "tes", "Test", "tes");
+        Map<String, Integer> result = new HashMap<>();
+        for (String word : words) {
+            if (result.containsKey(word)) {
+                result.put(word, result.get(word) + 1);
+            } else {
+                result.put(word, 1);
+            }
+        }
+        for (Map.Entry<String, Integer>entry : result.entrySet()){
+            if(entry.getValue()>1){
+                System.out.println(entry.getKey()+ "-"+entry.getValue());
             }
         }
     }
